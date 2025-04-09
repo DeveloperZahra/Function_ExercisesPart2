cb09016a6ad37fa28a01c2ab7a32d17896fbdd86
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-04-09 14:18:17 +0400

    menu

--- Program.cs
+++ Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to program menu \n 1. Check Even or Odd: \n 2. The Largest number is:  \n 3. Temperature in Fahrenheit: \n" +
+                " 4. the price of the item is: \n 5.The student's grade is:\n 6.Number after swapping:\n 7. Days to Weeks and Days Converter:\n" +
+                " 8. Electricity Bill Calculator is: \n 9. Simple Calculator:   ");
+            int choice = int.Parse(Console.ReadLine());
+
         }
     }
 }
